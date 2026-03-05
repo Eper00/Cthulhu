cd07b2a0302ce121c9b73dcc395a58a4874fa20a
Epres Tamás <104941550+Eper00@users.noreply.github.com>
2026-03-05 11:37:38 +0100

    Delete load.py

--- load.py
+++
@@ -1,9 +0,0 @@
-from utils.support import load_data
-from utils.parameters import dt
-from networks import train_dynamics,DynamicsModel
-inputs,states,next_states,outputs=load_data()
-dynamics_model=DynamicsModel()
-train_dynamics(dynamics_model,inputs,states,next_states)
-
-#mujoco.viewer.launch(model,data)
-
